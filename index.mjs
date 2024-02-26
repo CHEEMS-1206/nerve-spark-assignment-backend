8aabe3b4e8ac458dab49b1004451465499ea37d7
PRIYANSHU SINGH <sentysingh456@gmail.com>
2024-02-26 11:03:57 +0530

    started an express server

---
+++ index.mjs
@@ -0,0 +1,31 @@
+// Server entry point //
+
+// Necessary imports
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+// Constructed a server
+const SERVER = express();
+import initializeDatabase from './Config/config.mjs'
+import routers from "./Routes/routes.mjs";
+
+// Parsing the JSON, CORS policy, URL encoding, and JSON formatting
+SERVER.use(bodyParser.json());
+SERVER.use(cors());
+SERVER.use(express.urlencoded({ extended: true }));
+SERVER.use(express.json());
+
+// MIDDLEWARE
+SERVER.use("/api/admin",routers.adminRouter)
+SERVER.use("/api/user",routers.userRoutes)
+SERVER.use("/api/dealership",routers.dealershipRoutes)
+
+// Defining port for backend rest server
+const PORT = 5001;
+
+// Run the server at PORT
+SERVER.listen(PORT, () =>
+  console.log(`server running at: http://localhost:${PORT} `)
+);
+initializeDatabase().catch(console.error);
